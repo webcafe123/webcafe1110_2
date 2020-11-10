7d033dbafdd2a579e2a9dedff2ccc604aa7ad271
HGHG <HGHG@DESKTOP-HBI3NQT>
2020-11-10 09:08:08 +0900

    .

--- statement.sql
+++ statement.sql
@@ -1,4 +1,4 @@
---user
+--user_정보
 create table user_info (
   user_id               varchar2(100) primary key, --pk
   user_nickname         varchar2(100) not null,
@@ -10,33 +10,39 @@ create table user_info (
   user_del              char(1) default 'n',
   user_regdate          date default sysdate,
   user_update           date default sysdate,
-  user_pwd              VARCHAR2(100) not null
+  user_pwd              VARCHAR2(100) not null,
+  user_profile          VARCHAR2(50) not null
 );
-
-CREATE SEQUENCE AA START WITH 1 INCREMENT BY 1 MAXVALUE 300 CYCLE NOCACHE;
+ALTER TABLE user_info DROP COLUMN user_phone;
+ALTER TABLE user_info ADD (pwd varchar2(100)); 
+ALTER TABLE user_info RENAME COLUMN pwd TO user_pwd; 
+alter table user_info modify (user_email varchar2(200));
+alter table user_info modify (user_id varchar2(100)) ;
+alter table user_info modify (user_nickname varchar2(100)) ;
+alter table user_info modify (user_pwd varchar2(100)) ;
+desc user_info;
 --카테고리분류메뉴
 create table cate (
     c_num               NUMBER,                 --pk  key
-    c_subject           VARCHAR2(100) not null,  -- 카테고리명
+    c_subject           VARCHAR2(100) not null, -- 카테고리명
     c_id                varchar2(100),          --fk  생성자
     c_intro             VARCHAR2(100),          -- 카테고리 설명        
     c_regdate           DATE default sysdate,   -- 작성일
     c_del               CHAR(1) default 'n',    -- 삭제여부
-      ref                 NUMBER not null,        -- 카테끼리 그룹
-   c_step              NUMBER not null,        -- ref 내의 순서
-   c_level             NUMBER not null,        -- 들여쓰기
+    ref                 NUMBER not null,        -- 카테끼리 그룹
+    c_step              NUMBER not null,        -- ref 내의 순서
+    c_level             NUMBER not null,        -- 들여쓰기
     CONSTRAINT c_nm_pk PRIMARY KEY (c_num),
     CONSTRAINT c_id_fk FOREIGN KEY (c_id) REFERENCES user_info (user_id) 
 ); 
-
 --게시판
 create table board (
     b_num               NUMBER,                 --pk
     b_c_num             NUMBER not null,        --fk
-    b_subject           VARCHAR2(50) not null,
+    b_subject           VARCHAR2(100) not null,
     b_id                VARCHAR2(100) not null, --fk
     b_nick              VARCHAR2(100) not null,
-    b_content           VARCHAR2(1000),
+    b_content           VARCHAR2(4000),
     b_read_cnt          NUMBER,
     b_reply_cnt         NUMBER,
     b_like_cnt          NUMBER,
@@ -49,7 +55,10 @@ create table board (
     FOREIGN KEY (b_c_num) REFERENCES cate (c_num),
     FOREIGN KEY (b_id) REFERENCES user_info (user_id)
 );
-
+create table board_like (
+    b_like_num number REFERENCES board(b_num),
+    b_like_id varchar2(100) REFERENCES user_info(user_id)
+);
 --업로드 파일
 create table file_upload (
     f_num               NUMBER,                 --pk
@@ -59,16 +68,16 @@ create table file_upload (
     CONSTRAINT f_num_pk PRIMARY KEY (f_num),
     FOREIGN KEY (f_b_num) REFERENCES board (b_num)
 );
-
-
+--시퀀스
 create sequence file_upload_seq;
-
+--시퀀스 자동 숫자 조정
 create or replace function get_seq
     return number
 is
 begin
     return file_upload_seq.nextval;
 end;
+/
 -- 로그인 ip 기록
 CREATE table login_ip_history (
     i_num               NUMBER,                  --pk
@@ -78,9 +87,16 @@ CREATE table login_ip_history (
     CONSTRAINT i_num_pk PRIMARY KEY (i_num),
     FOREIGN KEY (i_id) REFERENCES user_info (user_id) 
 );
+--유희 시퀀스
 CREATE SEQUENCE seq_loginip
-START WITH 1
-INCREMENT BY 1;
+    START WITH 1
+    INCREMENT BY 1;
+--유정 시퀀스
+CREATE SEQUENCE AA 
+    START WITH 1 
+    INCREMENT BY 1 
+    MAXVALUE 300 
+    CYCLE NOCACHE;    
 -- 쪽지
 CREATE table message (
     m_num               NUMBER,                 --pk
@@ -98,12 +114,12 @@ CREATE table message (
     FOREIGN KEY (m_sender_id) REFERENCES user_info (user_id), 
     FOREIGN KEY (m_receiver_id) REFERENCES user_info (user_id) 
 );
-
+-- 댓글
 CREATE table reply (
     r_num               NUMBER,                 --pk
     r_b_num             NUMBER not null,        --fk
     r_ref               NUMBER not null,
-	r_origin			NUMBER not null,
+    r_origin            NUMBER not null,
     r_level             NUMBER not null,
     r_step              NUMBER not null,    
     r_id                varchar2(10) not null,  --fk
@@ -114,23 +130,4 @@ CREATE table reply (
     r_ip                VARCHAR2(20) not null,
     r_del               CHAR(1) default 'n',
     CONSTRAINT r_num_pk PRIMARY KEY (r_num)
-);     
-
-CREATE table reply (
-    r_num               NUMBER,                 --pk
-    r_b_num             NUMBER not null,        --fk
-    r_ref               NUMBER not null,
-	r_origin			NUMBER not null,
-    r_level             NUMBER not null,
-    r_step              NUMBER not null,    
-    r_id                varchar2(10) not null,  --fk
-    r_nick              varchar2(10) not null,
-    r_content           varchar2(500) not null,
-    r_regdate           DATE default sysdate,
-    r_update            DATE default sysdate,
-    r_ip                VARCHAR2(20) not null,
-    r_del               CHAR(1) default 'n',
-    CONSTRAINT r_num_pk PRIMARY KEY (r_num),
-    FOREIGN KEY (r_b_num) REFERENCES board (b_num), 
-    FOREIGN KEY (r_id) REFERENCES user_info (user_id) 
-);    
+);   
